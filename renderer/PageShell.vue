444cf0d1de504f1f25aabb61a493c1f13c6b66ca
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-09 14:44:09 +0000

    build(component): footer

--- renderer/PageShell.vue
+++ renderer/PageShell.vue
@@ -2,9 +2,14 @@
   <div class="p-4 md:p-16">
     <Header />
     <slot />
+    <LazyFooter />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { defineAsyncComponent } from 'vue'
 import Header from './Header.vue'
+const LazyFooter = defineAsyncComponent(() =>
+  import('./Footer.vue')
+)
 </script>
