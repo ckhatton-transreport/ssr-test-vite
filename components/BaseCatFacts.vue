e4e415a18d30940e9211983b8434e495708a10b6
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-25 14:18:24 +0000

    feat: add image from public

--- components/BaseCatFacts.vue
+++ components/BaseCatFacts.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="mb-4 p-4">
-    <h2 class="text-2xl">
-      Cat Facts 🐈
-    </h2>
-    <p class="my-4">
-      This grabs cat facts from an API <BaseLink
-        href="https://catfact.ninja/#/Facts/getFacts"
-        target="_blank"
+    <div class="flex flex-wrap justify-center">
+      <img
+        class="w-96 h-93 lg:mr-6 mb-6"
+        src="/img/kitten.jpeg"
+        alt="Kitten photo"
       >
-        (https://catfact.ninja/#/Facts/getFacts)
-      </BaseLink> via fetch.
-    </p>
-    <p
-      v-if="CatFactsStore.maxLength !== -1"
-      class="my-4"
-    >
-      The facts have been limited to a fact length of {{ CatFactsStore.maxLength }}.
-    </p>
+      <div>
+        <h2 class="text-2xl">
+          Cat Facts 🐈
+        </h2>
+        <p class="my-4">
+          This grabs cat facts from an API <BaseLink
+            href="https://catfact.ninja/#/Facts/getFacts"
+            target="_blank"
+          >
+            (https://catfact.ninja/#/Facts/getFacts)
+          </BaseLink> via fetch.
+        </p>
+        <p
+          v-if="CatFactsStore.maxLength !== -1"
+          class="my-4"
+        >
+          The facts have been limited to a fact length of {{ CatFactsStore.maxLength }}.
+        </p>
+      </div>
+    </div>
     <ToolPagination />
     <ToolFacts />
   </div>
