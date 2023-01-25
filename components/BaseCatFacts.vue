bbc54cea283975d40d14cd219e215ffb70538e2c
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-25 13:52:30 +0000

    build(component): tool/facts

--- components/BaseCatFacts.vue
+++ components/BaseCatFacts.vue
@@ -18,34 +18,14 @@
       The facts have been limited to a fact length of {{ CatFactsStore.maxLength }}.
     </p>
     <ToolPagination />
-    <section class="relative flex justify-center">
-      <div
-        :class="CatFactsStore.loading ? 'opacity-100' : 'opacity-0'"
-        class="loading"
-      >
-        <span class="text-xl">Loading</span>
-      </div>
-      <div
-        :class="{ 'opacity-40': CatFactsStore.loading }"
-        class="facts flex flex-wrap"
-      >
-        <div
-          v-for="(catFact, index) in CatFactsStore.facts"
-          :key="catFact.fact.split(' ')[0] + index"
-          class="w-full sm:w-1/2 md:w-1/3 xl:w-1/4"
-        >
-          <div class="bg-white rounded-lg shadow-lg m-4 p-4">
-            <h3>{{ catFact.fact }}</h3>
-          </div>
-        </div>
-      </div>
-    </section>
+    <ToolFacts />
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseLink from '/components/BaseLink.vue';
 import ToolPagination from '/components/ToolPagination.vue';
+import ToolFacts from '/components/ToolFacts.vue';
 
 import { onBeforeMount } from 'vue';
 import { CatFactsStore } from '../composables/CatFactsStore';
@@ -54,8 +34,3 @@ onBeforeMount((): void => {
   CatFactsStore.getFacts();
 });
 </script>
-
-<style lang="sass">
-.loading
-  @apply absolute top-12 bg-white rounded-lg shadow-lg p-4 z-10 transition-opacity duration-300
-</style>
